a3a50286c916d7301e6410edd21753d3733ed1cd
Chandanpreet Kaur <69481002+Chandanpreet91@users.noreply.github.com>
2022-03-26 12:16:54 -0700

    Some changes to main file.

--- main.py
+++ main.py
@@ -1,50 +1,82 @@
 from data import MENU 
 order = input("What would you like? (espresso/latte/cappuccino) :") 
-profit = 0
+
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
-def check_espresso(water, coffee, cost):
+def check_espresso():
     espresso_water = MENU["espresso"]["ingredients"]["water"]
     espresso_coffee = MENU["espresso"]["ingredients"]["coffee"]
-    espresso_cost = MENU["espresso"]["ingredients"]["cost"]
-    if  espresso_water == water or  espresso_water >= water:
-        if espresso_coffee == coffee or espresso_coffee >= coffee:
-            if espresso_coffee == cost or espresso_cost >= cost:
-                print("here's your coffe, Enjoy!")
+    if  espresso_water == resources["water"] or  espresso_water >= resources["water"]:
+        if espresso_coffee == resources["coffee"] or espresso_coffee >= resources["coffee"]:
+            resources["water"] -= espresso_coffee
+            resources["coffee"] -= espresso_coffee
+            print(" Enjoy!")
 
-def check_latte(water, milk, coffee, cost):
+def check_latte():
     latte_water = MENU["latte"]["ingredients"]["water"]
     latte_coffee = MENU["latte"]["ingredients"]["coffee"]
     latte_milk = MENU["latte"]["ingredients"]["milk"]
-    latte_cost = MENU["latte"]["ingredients"]["cost"]
-    if  latte_water == water or  latte_water >= water:
-        if  latte_coffee == coffee or latte_coffee >= coffee:
-            if latte_milk == milk or latte_milk >= milk:
-                if latte_coffee == cost or latte_cost >= cost:
-                    print("here's your coffe, Enjoy!")
-                    
-def check_cappuccino(water, milk, coffee, cost):
+    if  latte_water == resources["water"] or  latte_water >= resources["water"]:
+        if  latte_coffee == resources["water"] or latte_coffee >= resources["water"]:
+            if latte_milk == resources["water"] or latte_milk >= resources["water"]:
+                return True
+def check_cappuccino():
     cappuccino_water = MENU["cappuccino"]["ingredients"]["water"]
     cappuccino_coffee = MENU["cappuccino"]["ingredients"]["coffee"]
-    cappuccino_cost = MENU["cappuccino"]["ingredients"]["cost"]
     cappuccino_milk = MENU["cappuccino"]["ingredients"]["milk"]
-    if  cappuccino_water == water or  cappuccino_water >= water:
-        if  cappuccino_coffee == coffee or cappuccino_coffee >= coffee:
-            if cappuccino_milk == milk or cappuccino_milk >= milk:
-                if cappuccino_coffee == cost or cappuccino_cost >= cost:
-                    print("here's your coffe, Enjoy!")
+    if  cappuccino_water == resources["water"] or  cappuccino_water >= resources["water"]:
+        if  cappuccino_coffee == resources["coffee"] or cappuccino_coffee >= resources["coffee"]:
+            if cappuccino_milk == resources["milk"] or cappuccino_milk >= resources["milk"]:
+                return True
+
+def check_coins(cent,quarter, nickle, dollar):
+  profit = 0
+  espresso_cost = MENU["espresso"]["cost"]
+  latte_cost = MENU["latte"]["cost"] 
+  cappuccino_cost = MENU["cappuccino"]["cost"] 
+  total =  espresso_cost+latte_cost+cappuccino_cost
+  total_receive = cent+quarter+nickle+dollar            
+  if total_receive == total:
+       profit += total_receive
+       return print("equal")
+  elif total_receive >= total:
+      owe_amount = total_receive - total
+      profit += total
+      return f"{owe_amount} return money"
+  elif total_receive <= total:
+      print('You don\'t have enough money. Here\'s is your refund.')
 
 if order == "report":
-    for resource in resources:
+    for resource in resources["water"]:
         print(f"{resource }: {resources[resource]}")
-    print(f"Money: {profit}")
+    
 elif order == 'espresso':
-    print()
+    cent = float(input("Enter some cents: "))
+    quarter = float(input("Enter some Quarters : "))
+    nickle = float(input("Enter a nickel: "))
+    dollar = float(input("Enter a dollar: "))
+    
+    if check_coins(cent,quarter,nickle,dollar):
+        check_espresso()
+    # if check_espresso(): 
+    #     check_coins(cent,quarter,nickle,dollar)
+    #     print("Enjoy!")
 elif order == 'latte':
+    cent = float(input("Enter some cents: "))
+    quarter = float(input("Enter some Quarters : "))
+    nickle = float(input("Enter a nickel: "))
+    dollar = float(input("Enter a dollar: "))
     print()
 elif order == 'cappuccino':
+    cent = float(input("Enter some cents: "))
+    quarter = float(input("Enter some Quarters : "))
+    nickle = float(input("Enter a nickel: "))
+    dollar = float(input("Enter a dollar: "))
     print()
+
+
